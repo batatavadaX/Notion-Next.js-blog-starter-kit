3520792e25bd3d162cbaf2d2f665eda63450bb7d
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2023-10-15 13:47:04 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,31 +1,32 @@
-import { siteConfig } from './lib/site-config';
+import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'c5ec6f61b4a44e509fa9c1210a06692a',
+  rootNotionPageId: 'f1c8015c8dd847219294f80863b7802e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'atropa',
-  domain: '',
-  author: '',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: '',
+  description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
-  // github: '',
-  // linkedin: 'fisch2',
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon:'',
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -49,33 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'a',
-      pageId: 'c5ec6f61b4a44e509fa9c1210a06692a',
-    },
-    {
-      title: 'b',
-      pageId: 'c5ec6f61b4a44e509fa9c1210a06692a',
-      menuPage: true,
-    },
-  ],
-
-  // -------- custom configs (2skydev) -------------
-
-  // date-fns format string
-  dateformat: 'yyyy MM dd',
-
-  // post page - hidden properties
-  hiddenPostProperties: ['', '', ' '],
-
-  // contentPosition (table of contents) text align
-  contentPositionTextAlign: 'left',
-
-  // default theme color
-  defaultTheme: 'system',
-
-  // enable comment
-  enableComment: false,
-});
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
