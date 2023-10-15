f546fc10f44e29cfe7fc63df032ac1b0c5c4e1b9
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2023-10-15 13:50:46 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,32 +1,31 @@
-import { siteConfig } from './lib/site-config'
+import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f1c8015c8dd847219294f80863b7802e',
+  rootNotionPageId: '7875426197cf461698809def95960ebf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'atropa',
+  domain: '',
+  author: '',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // twitter: 'transitive_bs',
+  // github: '',
+  // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon:'',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -50,16 +49,33 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
-})
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'a',
+      pageId: '7875426197cf461698809def95960ebf',
+    },
+    {
+      title: 'b',
+      pageId: '7875426197cf461698809def95960ebf',
+      menuPage: true,
+    },
+  ],
+
+  // -------- custom configs (2skydev) -------------
+
+  // date-fns format string
+  dateformat: 'yyyy MM dd',
+
+  // post page - hidden properties
+  hiddenPostProperties: ['', '', ' '],
+
+  // contentPosition (table of contents) text align
+  contentPositionTextAlign: 'left',
+
+  // default theme color
+  defaultTheme: 'system',
+
+  // enable comment
+  enableComment: false,
+});
