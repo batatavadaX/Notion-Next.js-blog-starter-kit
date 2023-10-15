593cbf2103ea81a5daf83dbc81deb07d1fc774c4
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2023-10-15 12:33:05 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,24 +9,23 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '2skydev blog',
-  domain: 'blog.2skydev.com',
-  author: '2skydev',
+  name: 'atropa',
+  domain: '',
+  author: '',
 
   // open graph metadata (optional)
-  description: '2skydev blog - developer blog',
+  description: '',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: '2skydev',
+  // github: '',
   // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon:
-    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d2daa37-61d0-45b6-b333-9a2bd0bdc3ee%2Fprofile_%25E1%2584%2580%25E1%2585%25A9%25E1%2584%2592%25E1%2585%25AA%25E1%2584%258C%25E1%2585%25B5%25E1%2586%25AF_circle.png?table=block&id=d1e89e9e-42eb-4ebf-9486-ae0374039efc&spaceId=2eb5336b-2edb-42d0-bc6c-95d72d4d1b74&width=250&userId=bef10e95-202b-4b6b-9626-7af866b6f9ba&cache=v2',
+  defaultPageIcon:'',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -53,12 +52,12 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      title: '',
+      pageId: '',
     },
     {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      title: '',
+      pageId: '',
       menuPage: true,
     },
   ],
@@ -66,10 +65,10 @@ export default siteConfig({
   // -------- custom configs (2skydev) -------------
 
   // date-fns format string
-  dateformat: 'yyyy년 MM월 dd일',
+  dateformat: 'yyyy MM dd',
 
   // post page - hidden properties
-  hiddenPostProperties: ['설명', '상태', '최하위 정렬'],
+  hiddenPostProperties: ['', '', ' '],
 
   // contentPosition (table of contents) text align
   contentPositionTextAlign: 'left',
@@ -78,5 +77,5 @@ export default siteConfig({
   defaultTheme: 'system',
 
   // enable comment
-  enableComment: true,
+  enableComment: false,
 });
